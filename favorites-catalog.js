c5b5b173ded22a834e9dca4a818affade5099f21
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-05-11 18:25:42 -0400

    Update and rename favorite-catalog.js to favorites-catalog.js

--- favorites-catalog.js
+++ favorites-catalog.js
@@ -56,50 +56,50 @@ function updateCatalog() {
       }
 
       const favoriteBtns = document.querySelectorAll('.favorite-btn');
-      favoriteBtns.forEach(favoriteBtn => {
-      favoriteBtn.addEventListener('click', () => {
+      favoriteBtns.forEach(function(favoriteBtn) {
+        favoriteBtn.addEventListener('click', function() {
           const user = firebase.auth().currentUser;
-        if (!user) {
-          // Prompt user to log in or sign up
-          window.location.replace('/sign-up');
-          return;
-        }
-
-        const uid = user.uid;
-
-        const card = this.closest('.card');
-        const productId = card.dataset.productId;
-        const favoritesRef = firebase.firestore().collection('favorites').doc(uid);
-        favoritesRef.get().then(doc => {
-          if (doc.exists && doc.data().products.includes(productId)) {
-            // Product is already in favorites; remove it
-            favoritesRef.update({
-              products: firebase.firestore.FieldValue.arrayRemove(productId)
-            }).then(() => {
-              // Update button image
-              this.classList.remove('favorited');
-            });
-          } else {
-            // Product is not in favorites; add it
-            favoritesRef.set({
-              products: firebase.firestore.FieldValue.arrayUnion(productId)
-            }, { merge: true }).then(() => {
-              // Update button image
-              this.classList.add('favorited');
-            });
+          if (!user) {
+            // Prompt user to log in or sign up
+            window.location.replace('/sign-up');
+            return;
           }
-        });
 
-        const giftId = card.dataset.giftId;
-        const giftsRef = firebase.firestore().collection('gifts');
-        const giftRef = giftsRef.doc(giftId);
+          const uid = user.uid;
 
-        giftRef.update({
-          favorite: true,
-        }).then(() => {
-          console.log("Gift marked as favorite");
-        }).catch((error) => {
-          console.error("Error marking gift as favorite: ", error);
+          const card = this.closest('.card');
+          const productId = card.dataset.productId;
+          const favoritesRef = firebase.firestore().collection('favorites').doc(uid);
+          favoritesRef.get().then(doc => {
+            if (doc.exists && doc.data().products.includes(productId)) {
+              // Product is already in favorites; remove it
+              favoritesRef.update({
+                products: firebase.firestore.FieldValue.arrayRemove(productId)
+              }).then(() => {
+                // Update button image
+                this.classList.remove('favorited');
+              });
+            } else {
+              // Product is not in favorites; add it
+              favoritesRef.set({
+                products: firebase.firestore.FieldValue.arrayUnion(productId)
+              }, { merge: true }).then(() => {
+                // Update button image
+                this.classList.add('favorited');
+              });
+            }
+          });
+
+          const giftId = card.dataset.giftId;
+          const giftsRef = firebase.firestore().collection('gifts');
+          const giftRef = giftsRef.doc(giftId);
+
+          giftRef.update({
+            favorite: true,
+          }).then(() => {
+            console.log("Gift marked as favorite");
+          }).catch((error) => {
+            console.error("Error marking gift as favorite: ", error);
+          });
         });
-        });
-     });
+      });
