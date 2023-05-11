c377bc422a7830d03c90e716e7bbb33ee1d207d9
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-05-11 17:37:16 -0400

    Update and rename favorites.js to favorite.js

--- favorite.js
+++ favorite.js
@@ -1,7 +1,5 @@
-
 const favoriteBtn = document.getElementById('favorite-btn');
 favoriteBtn.addEventListener('click', toggleFavorite);
-
 
 function toggleFavorite() {
   const user = firebase.auth().currentUser;
@@ -12,9 +10,9 @@ function toggleFavorite() {
   }
 
   const uid = user.uid;
-
   const productId = favoriteBtn.dataset.productId;
   const favoritesRef = firebase.firestore().collection('favorites').doc(uid);
+
   favoritesRef.get().then(doc => {
     if (doc.exists && doc.data().products.includes(productId)) {
       // Product is already in favorites; remove it
@@ -23,6 +21,13 @@ function toggleFavorite() {
       }).then(() => {
         // Update button image
         favoriteBtn.classList.remove('favorited');
+
+        // Unmark gift as favorite
+        const giftId = favoriteBtn.dataset.giftId;
+        const giftsRef = firebase.firestore().collection('gifts');
+        giftsRef.doc(giftId).update({ favorite: false })
+          .then(() => console.log("Gift unmarked as favorite"))
+          .catch(error => console.error("Error unmarking gift as favorite: ", error));
       });
     } else {
       // Product is not in favorites; add it
@@ -31,7 +36,15 @@ function toggleFavorite() {
       }, { merge: true }).then(() => {
         // Update button image
         favoriteBtn.classList.add('favorited');
+
+        // Mark gift as favorite
+        const giftId = favoriteBtn.dataset.giftId;
+        const giftsRef = firebase.firestore().collection('gifts');
+        giftsRef.doc(giftId).update({ favorite: true })
+          .then(() => console.log("Gift marked as favorite"))
+          .catch(error => console.error("Error marking gift as favorite: ", error));
       });
     }
   });
 }
+
