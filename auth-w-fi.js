e9b1ee65707b43a82b0d347407391b70ff96b0ff
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-05-13 12:39:16 -0400

    Update and rename auth-w-f.js to auth-w-fi.js

--- auth-w-fi.js
+++ auth-w-fi.js
@@ -11,7 +11,6 @@ firebase.analytics && firebase.analytics();
   var userDisplayName = document.querySelectorAll('[data-user-displayName]');
   var userEmail = document.querySelectorAll('[data-user-email]');
   var userContent = document.querySelectorAll('[data-user]');
-  var userName = document.querySelectorAll('[data-user-name]');
 
   userAuth.forEach(function(el) { el.style.display = 'none'; });
   userUnauth.forEach(function(el) { el.style.display = 'none'; });
@@ -42,12 +41,11 @@ firebase.analytics && firebase.analytics();
       userUnauth.forEach(function(el) { el.style.display = 'none'; });
 
       userEmail.forEach(function(el) { el.innerText = user.email; });
-      userName.forEach(function(el) { el.innerText = user.name; });
       userDisplayName.forEach(function(el) { el.innerText = user.displayName; });
       
       firebase.firestore().collection("users").doc(user.uid).set({
         email: user.email,
-        name: user.name,
+        name: user.displayName,
         favorites: []
       })
     } else {
