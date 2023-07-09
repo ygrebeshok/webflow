d6009b3aca8c8a1b10dd254c05986c3ab3f89c87
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-07-09 16:32:53 -0400

    Update and rename upd-catalog-10.js to upd-catalog-11.js

--- upd-catalog-11.js
+++ upd-catalog-11.js
@@ -106,7 +106,8 @@ function updateCatalog() {
         card.querySelector("#price").textContent = `$${data.price}`,
         card.querySelector("#description").textContent = data.description,
         card.querySelector("#keywords").textContent = data.openai_keywords + ',' + data.image_labels,
-        card.querySelector("#link-container").href = data.product_link
+        card.querySelector("#link-container").href = data.product_link,
+        card.querySelector("#brand").textContent = data.brand
 
         allCards.push(card);
         
