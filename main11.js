573a5181a092ea4d31d6269da502c2d33c32e4ef
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-07-19 22:56:27 -0400

    Update main11.js

--- main11.js
+++ main11.js
@@ -9,19 +9,19 @@
       const holidays = ["Birthday", "Christmas", "Thanksgiving", "Valentine's", "Mother's Day", "Easter", "Graduation", "Wedding", "Anniversary"];
       holidays.forEach((holiday) => {
     	const button = document.createElement('button');
-      button.textContent = holiday;
-      button.className = 'button';
-      button.addEventListener('mouseenter', () => {
-      	button.classList.add('hover');
-      });
-  		button.addEventListener('mouseleave', () => {
-      	button.classList.remove('hover');
-      });
-      button.addEventListener('focus', function() {
-    		button.classList.add('focus');
-        selected_holiday = button.textContent;
-  		});
-      holidayGrid.appendChild(button);
+        button.textContent = holiday;
+        button.className = 'button';
+        button.addEventListener('mouseenter', () => {
+      	  button.classList.add('hover');
+        });
+  	button.addEventListener('mouseleave', () => {
+      	  button.classList.remove('hover');
+        });
+        button.addEventListener('focus', function() {
+    	  button.classList.add('focus');
+          selected_holiday = button.textContent;
+  	});
+        holidayGrid.appendChild(button);
       });
 
     for (let i = holidays.length; i < 8; i++) {
@@ -40,31 +40,32 @@
     
     resetBrandFilters();
     
-  	document.getElementById("textarea").disabled = true;
+    document.getElementById("textarea").disabled = true;
     errorAlert.style.visibility = "hidden";
     holidayGrid.classList.add('disablegrid');
     searchAgain.style.visibility = "hidden";
     filtersContainer.style.visibility = "hidden";
     document.getElementById("textarea").style.color = "black";
     lottieLoader.style.visibility = "visible";
-  	const text = document.getElementById("textarea").value;
+    const text = document.getElementById("textarea").value;
 		
-  	try {
-    	const response = await fetch("https://api.openai.com/v1/engines/text-davinci-003/completions", {
-     	method: "POST",
-      headers: {
-      	"Content-Type": "application/json",
-       	"Authorization": `Bearer ${openaiApiKey}`
-      },
-      body: JSON.stringify({
-        prompt: "Give some gift recommendations for " + selected_holiday + "\n" + "Here is the case: " + text,
-        max_tokens: 1024,
-        temperature: 0.5,
-        top_p: 1,
-        frequency_penalty: 0,
-        presence_penalty: 0
-      })
-    });
+    try {
+      const response = await fetch("https://api.openai.com/v1/engines/text-davinci-003/completions", {
+        method: "POST",
+        headers: {
+      	  "Content-Type": "application/json",
+       	  "Authorization": `Bearer ${openaiApiKey}`
+        },
+        body: JSON.stringify({
+          prompt: "Give some gift recommendations for " + selected_holiday + "\n" + "Here is the case: " + text,
+          max_tokens: 1024,
+          temperature: 0.5,
+          top_p: 1,
+          frequency_penalty: 0,
+          presence_penalty: 0
+        })
+      });
+
     const data = await response.json();
     const responseText = data.choices[0].text;
 
@@ -81,8 +82,9 @@
         top_p: 1,
         frequency_penalty: 0,
         presence_penalty: 0
-      	})
-    	});
+      })
+    });
+
     const data2 = await response2.json();
     let keywords = data2.choices[0].text;
     const newKeywords = keywords.replace(/\n/g, '');
@@ -92,14 +94,14 @@
     keywords = keywords.filter(keyword => keyword !== "");
 
     if (keywords.length > 0) {
-  		if (keywords[0].charAt(0) == " ") {
-    		keywords[0] = keywords[0].substring(1);
-  		}
+      if (keywords[0].charAt(0) == " ") {
+    	  keywords[0] = keywords[0].substring(1);
+      }
 
-  		if (keywords[0].startsWith("\n\n")) {
-    		keywords[0] = keywords[0].substring(2);
-  		}
-		}
+      if (keywords[0].startsWith("\n\n")) {
+        keywords[0] = keywords[0].substring(2);
+      }
+    }
 
     const openaiKeywords = new Set(keywords);
     let visibleCards = [];
@@ -111,6 +113,7 @@
       cardKeywords = cardKeywords.filter(keyword => keyword !== "");
       const cardKeywordsSet = new Set(cardKeywords);
       const intersection = new Set([...openaiKeywords].filter(x => cardKeywordsSet.has(x)));
+      
       if (intersection.size === 0) {
         card.style.display = "none";
       } else {
@@ -196,7 +199,7 @@
   }
 
   function checkInputs() {
-  	document.getElementById("button-container").classList.remove('disablegrid');
+    document.getElementById("button-container").classList.remove('disablegrid');
     mainButton.classList.remove('disablegrid');
   }
 
