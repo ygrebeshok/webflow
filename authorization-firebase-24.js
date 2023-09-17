8989c4f8649eaaeb696e84ba420701ab0f32055b
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-09-17 01:24:12 -0400

    Update and rename authorization-firebase-23.js to authorization-firebase-24.js

--- authorization-firebase-24.js
+++ authorization-firebase-24.js
@@ -117,14 +117,5 @@ function logout() {
     });
 }
 
-function resetPassword(email, successReset) {
-  firebase.auth().sendPasswordResetEmail(email)
-    .then(function() {
-      successReset.textContent = "Email sent. Please, Check your inbox.";
-    })
-    .catch(function(error) {
-      console.error("Error sending password reset email:", error);
-    });
-}
 
 }
