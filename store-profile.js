5e3cb3b0a32ca20ac785c257e67ca7634c294347
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-09-24 02:31:33 -0400

    Update store-profile.js

--- store-profile.js
+++ store-profile.js
@@ -1,1 +1,13 @@
+var authLogout = document.querySelectorAll('[store-logout]');
 
+authLogout.forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    firebase.auth().signOut().then(function() {
+      user = null;
+      window.location.href = webflowAuth.logoutRedirectPath;
+    });
+  });
+});
