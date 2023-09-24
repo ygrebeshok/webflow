35275993f3655a1d35be88b48fc34d16db8e4c76
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-09-23 21:24:01 -0400

    Update and rename authorization-store-17.js to authorization-store-18.js

--- authorization-store-18.js
+++ authorization-store-18.js
@@ -129,7 +129,19 @@ firebase.analytics && firebase.analytics();
       firebase.auth().signInWithEmailAndPassword(loginEmail.value, loginPassword.value)
       .then(function(authUser) {
         user = authUser;
-        window.location.href = webflowAuth.loginRedirectPath;
+        if (user) {
+          const storesRef = firebase.firestore().collection('stores');
+          storesRef.doc(user.uid).get().then((doc) => {
+            if (!doc.exists) {
+	            firebase.auth().signOut().then(function() {
+                user = null;
+                window.location.href = webflowAuth.signupPath;
+              })
+            } else {
+              window.location.href = webflowAuth.loginRedirectPath;
+            }
+          });
+        }
       })
       .catch(function(error) {
         loginErrors.forEach(function(el) {
@@ -139,4 +151,19 @@ firebase.analytics && firebase.analytics();
       });
     });
   });
+
+  
+var authLogout = document.querySelectorAll('[store-logout]');
+
+authLogout.forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    firebase.auth().signOut().then(function() {
+      user = null;
+      window.location.href = webflowAuth.logoutRedirectPath;
+    });
+  });
+});
 }
