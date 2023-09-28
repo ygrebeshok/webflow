272f239ff05777dcde46110a6d366bafbb1987c6
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-09-28 18:11:30 -0400

    Update and rename store-profile-79.js to store-profile-80.js

--- store-profile-80.js
+++ store-profile-80.js
@@ -189,7 +189,7 @@ firebase.auth().onAuthStateChanged(function(authUser) {
              })
 	     .then(() => {
     	       // Load products after updating the brand
-    	       loadProducts(newStoreName);
+    	       loadProducts(storeNameValue);
   	     })
              .catch((error) => {
                console.error('Error updating brands in gifts collection: ', error);
