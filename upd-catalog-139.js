a41c81668697c41aa028c818cfd01105ac4d8d23
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-10-01 19:04:29 -0400

    Update and rename upd-catalog-138.js to upd-catalog-139.js

--- upd-catalog-139.js
+++ upd-catalog-139.js
@@ -507,15 +507,24 @@ function updateCatalog() {
 
       catalogGrid.appendChild(card);
 
-      var grid = new Muuri(catalogGrid, {
-        items: card,
-        layout: {
-          fillGaps: true,
-          horizontal: false,
-          alignRight: false,
-          alignBottom: false,
-          rounding: true
+      interact(card)
+      .draggable({
+        inertia: true,
+        restrict: {
+          restriction: "parent",
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
         }
+      })
+      .on('dragmove', function (event) {
+        var target = event.target;
+        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
+        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
+
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
       });
 
       card.addEventListener('mouseenter', () => {
