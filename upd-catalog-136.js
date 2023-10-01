bc239b60baaa50840e56c8286f18f3a7be4cc2df
ygrebeshok <41209018+ygrebeshok@users.noreply.github.com>
2023-10-01 18:54:25 -0400

    Update and rename upd-catalog-135.js to upd-catalog-136.js

--- upd-catalog-136.js
+++ upd-catalog-136.js
@@ -528,26 +528,6 @@ function updateCatalog() {
         });
       });
 
-      interact(card)
-      .draggable({
-        inertia: true,
-        restrict: {
-          restriction: "parent",
-          endOnly: true,
-          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-        }
-      })
-      .on('dragmove', function (event) {
-        var target = event.target;
-        var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx;
-        var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
-        target.style.transform = 'translate(' + x + 'px, ' + y + 'px)';
-
-        target.setAttribute('data-x', x);
-        target.setAttribute('data-y', y);
-      });
-
       var brands = Array.from(brandsSet); // add this line
       populateBrandFilter(brands);
 
